bb13048d084da3a41502284662e85e22ce617a84
Dhiraj Dhas <dhiraj.dhas@coreviewsystems.com>
2023-04-17 15:21:21 +0530

    first commit

---
+++ suma/sumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sumaApp.swift
+//  suma
+//
+//  Created by sumasoft on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct sumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
